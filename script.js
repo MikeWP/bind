8196a56ad0952c971c682db02274dfd92b579379
Michal Soltys <michal@piekne-strony.pl>
2020-04-01 16:34:26 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var person = {
+    firstName : 'John',
+    lastName : 'Doe',
+    getFullName : function() {
+        return `${this.firstName} ${this.lastName}`
+    }
+}
+console.log(person.getFullName())
+
+var henry = {
+    firstName : 'Henry',
+    lastName : 'kramel',
+}
+
+console.log(henry)
+
+var getHenryFullName = person.getFullName.bind(henry)
+
+console.log(getHenryFullName);
